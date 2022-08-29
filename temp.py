0b9909e583bd285d264b55aa32e6ef14731188cb
UlissesMattos <ulisseslmattos@gmail.com>
2022-08-28 21:59:59 -0300

    Template completo
    Um script para envio de e-mails feito em python.

---
+++ temp.py
@@ -0,0 +1,37 @@
+from string import Template
+from datetime import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+import smtplib
+
+meu_email = 'SEUEMAIL@GMAIL.COM'
+minha_senha = 'SUASENHA'
+
+with open('template.html', 'r', encoding='utf-8') as html:
+    template = Template(html.read())
+    data_atual = datetime.now().strftime('%d/%m/%Y')
+    corpo_msg = template.substitute(nome='Ulisses', data= data_atual)
+
+msg = MIMEMultipart()
+msg['from'] = 'SEU NOME'
+msg['to'] = 'EMAILDOCLIENTE@GMAIL.COM' #Cliente
+msg['subject'] = 'ASSUNTO DO EMAIL'
+
+corpo = MIMEText(corpo_msg, 'html')
+msg.attach(corpo)
+
+with open('imagem.jpg', 'rb') as img:
+    img = MIMEImage(img.read())
+    msg.attach(img)
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    try:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login(meu_email, minha_senha)
+        smtp.send_message(msg)
+        print('E-mail enviado com sucesso.')
+    except Exception as e:
+        print('Erro', e)
+        
